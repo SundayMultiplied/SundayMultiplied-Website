d1a309e585b434789e58729a48a25ca6d084b1df
SundayMultiplied <brian@sundaymultiplied.com>
2026-08-30 12:04:24 -0400

    Move revision workspace off approvals page

--- app/approvals/page.tsx
+++ app/approvals/page.tsx
@@ -1,13 +1,9 @@
 import { ApprovalDashboard } from "../../components/approval-dashboard";
-import { RevisionQueue } from "../../components/revision-queue";
 import { requireChatGPTUser } from "../chatgpt-auth";
 
 export const dynamic = "force-dynamic";
 
 export default async function ApprovalsPage() {
   await requireChatGPTUser("/approvals");
-  return <>
-    <RevisionQueue />
-    <ApprovalDashboard />
-  </>;
+  return <ApprovalDashboard />;
 }
