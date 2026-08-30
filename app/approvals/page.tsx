29da8a5cbe741ce70acacd057ba35691152716e7
SundayMultiplied <brian@sundaymultiplied.com>
2026-08-29 22:58:45 -0400

    Surface revision queue on approval dashboard

--- app/approvals/page.tsx
+++ app/approvals/page.tsx
@@ -1,9 +1,13 @@
 import { ApprovalDashboard } from "../../components/approval-dashboard";
+import { RevisionQueue } from "../../components/revision-queue";
 import { requireChatGPTUser } from "../chatgpt-auth";
 
 export const dynamic = "force-dynamic";
 
 export default async function ApprovalsPage() {
   await requireChatGPTUser("/approvals");
-  return <ApprovalDashboard />;
+  return <>
+    <RevisionQueue />
+    <ApprovalDashboard />
+  </>;
 }
