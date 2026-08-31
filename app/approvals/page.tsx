b50ef13c758ae9179839d62353c4517956c9c640
SundayMultiplied <brian@sundaymultiplied.com>
2026-08-31 09:55:38 -0400

    Make approvals page approval-history only

--- app/approvals/page.tsx
+++ app/approvals/page.tsx
@@ -1,9 +1,9 @@
-import { ApprovalDashboard } from "../../components/approval-dashboard";
+import { ApprovalPackagesDashboard } from "../../components/approval-packages-dashboard";
 import { requireChatGPTUser } from "../chatgpt-auth";
 
 export const dynamic = "force-dynamic";
 
 export default async function ApprovalsPage() {
   await requireChatGPTUser("/approvals");
-  return <ApprovalDashboard />;
+  return <ApprovalPackagesDashboard />;
 }
